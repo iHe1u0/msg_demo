dcba2217e4655c6c6461cda340f5f3bda55dffa6
coder-cc <coder-cc@outlook.com>
2024-08-12 01:17:07 +0800

    update demo

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,37 @@ mod libs;
 
 fn main() -> io::Result<()> {
     let file_path = libs::file_helper::get_cache_path();
-    let data = vec!["Hello", "world", "this", "is", "Rust"];
-    libs::file_helper::save_data(&file_path, data)?;
+
+    //create a test json vec
+    let mut msg_vec = Vec::new();
+
+    for msg_id in 1..10 {
+        msg_vec.push(Message {
+            msg_id: msg_id,
+            msg: "test".to_string(),
+            from_user: msg_id % 2 == 0,
+        });
+    }
+
+    // then, transform it into a vec<&str>
+    let mut messages: Vec<String> = Vec::new();
+    for message in msg_vec {
+        let msg_json: String = serde_json::to_string(&message)?;
+        messages.push(msg_json);
+    }
+
+    // Create a Vec<&str> from Vec<String>
+    let message_refs: Vec<&str> = messages.iter().map(AsRef::as_ref).collect();
+
+    libs::file_helper::save_data(&file_path, message_refs)?;
     println!("Appended strings to file.");
 
     let strings = libs::file_helper::read_data(&file_path)?;
-    for s in strings {
-        println!("{}", s);
+
+    for msg in strings {
+        let un_json: Message = serde_json::from_str(&msg)?;
+        println!("{:?}", un_json.msg);
     }
-
-    let msg = Message {
-        msg_id: 1,
-        msg: "Hello 测试文档".to_string(),
-        from_user: true,
-    };
-    let data = serde_json::to_string(&msg)?;
-    // println!("{:?}", data);
-
-    let un_json: Message = serde_json::from_str(&data)?;
-    println!("{:?}", un_json);
 
     Ok(())
 }
